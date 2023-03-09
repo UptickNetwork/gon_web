c0ce200268f1a85e2f44fd303e0acc918dc066ce
fan <447690182@qq.com>
2023-03-09 14:33:23 +0800

    comint

---
+++ dashbordweb/gon-dashboard/src/views/dashboard/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import {
+  mapGetters
+} from 'vuex'
+import {
+  getDashboradHome
+} from '@/api/user'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  mounted() {
+    console.log(112232344)
+    this.getDashboradHome()
+  },
+  methods: {
+    getDashboradHome() {
+      this.fullscreenLoading = true
+      getDashboradHome().then(response => {
+        this.fullscreenLoading = false
+        console.log(response.data)
+        this.groupData = response.data
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .dashboard {
+    &-container {
+      margin: 30px;
+    }
+
+    &-text {
+      font-size: 30px;
+      line-height: 46px;
+    }
+  }
+</style>
